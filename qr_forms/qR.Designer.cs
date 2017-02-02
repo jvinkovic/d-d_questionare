53cd45dd698b3d1b21b21b470d29545e8afe4054
jvinkovic <josip.vinkovic@span.eu>
2017-02-02 11:55:06 +0100

    added project

---
+++ qr_forms/qR.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace qr_forms
+{
+    partial class qR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(qR));
+            this.btnTemplate = new System.Windows.Forms.Button();
+            this.pTemplate = new System.Windows.Forms.Panel();
+            this.lblSetCorrect = new System.Windows.Forms.Label();
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbQ = new System.Windows.Forms.TextBox();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.lblStatus0 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pTemplate.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTemplate
+            // 
+            this.btnTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTemplate.Location = new System.Drawing.Point(770, 13);
+            this.btnTemplate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTemplate.Name = "btnTemplate";
+            this.btnTemplate.Size = new System.Drawing.Size(259, 28);
+            this.btnTemplate.TabIndex = 4;
+            this.btnTemplate.Text = "StartButton";
+            this.btnTemplate.UseVisualStyleBackColor = true;
+            this.btnTemplate.Visible = false;
+            // 
+            // pTemplate
+            // 
+            this.pTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pTemplate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pTemplate.Controls.Add(this.lblStatus);
+            this.pTemplate.Location = new System.Drawing.Point(481, 2);
+            this.pTemplate.Margin = new System.Windows.Forms.Padding(4);
+            this.pTemplate.Name = "pTemplate";
+            this.pTemplate.Size = new System.Drawing.Size(281, 39);
+            this.pTemplate.TabIndex = 4;
+            this.pTemplate.Visible = false;
+            // 
+            // lblSetCorrect
+            // 
+            this.lblSetCorrect.AutoSize = true;
+            this.lblSetCorrect.Location = new System.Drawing.Point(137, -3);
+            this.lblSetCorrect.Name = "lblSetCorrect";
+            this.lblSetCorrect.Size = new System.Drawing.Size(62, 16);
+            this.lblSetCorrect.TabIndex = 16;
+            this.lblSetCorrect.Text = "                  ";
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.Location = new System.Drawing.Point(968, 12);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(84, 29);
+            this.btnShuffle.TabIndex = 16;
+            this.btnShuffle.Text = "Shuffle";
+            this.btnShuffle.UseVisualStyleBackColor = true;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(71, 18);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 16);
+            this.lblStatus.TabIndex = 14;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(363, 15);
+            this.btnGet.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(111, 28);
+            this.btnGet.TabIndex = 11;
+            this.btnGet.Text = "Get questions";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "What questions?";
+            // 
+            // tbQ
+            // 
+            this.tbQ.Location = new System.Drawing.Point(140, 17);
+            this.tbQ.Margin = new System.Windows.Forms.Padding(4);
+            this.tbQ.Name = "tbQ";
+            this.tbQ.Size = new System.Drawing.Size(213, 22);
+            this.tbQ.TabIndex = 13;
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.ForeColor = System.Drawing.Color.Red;
+            this.lblEnd.Location = new System.Drawing.Point(771, 21);
+            this.lblEnd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(0, 16);
+            this.lblEnd.TabIndex = 14;
+            // 
+            // lblStatus0
+            // 
+            this.lblStatus0.AutoSize = true;
+            this.lblStatus0.Location = new System.Drawing.Point(771, 2);
+            this.lblStatus0.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus0.Name = "lblStatus0";
+            this.lblStatus0.Size = new System.Drawing.Size(0, 16);
+            this.lblStatus0.TabIndex = 15;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 400;
+            this.toolTip1.IsBalloon = true;
+            // 
+            // qR
+            // 
+            this.AcceptButton = this.btnGet;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1064, 506);
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.lblSetCorrect);
+            this.Controls.Add(this.lblStatus0);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.tbQ);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.pTemplate);
+            this.Controls.Add(this.btnTemplate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(1080, 545);
+            this.Name = "qR";
+            this.Text = "qR";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mM);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mU);
+            this.pTemplate.ResumeLayout(false);
+            this.pTemplate.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTemplate;
+        private System.Windows.Forms.Panel pTemplate;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbQ;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Label lblStatus0;
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.Label lblSetCorrect;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ qr_forms/qR.cs
@@ -0,0 +1,382 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace qr_forms
+{
+    public partial class qR : Form
+    {
+        private Button currb;
+        private int initX;
+        private int initY;
+        private int mL;
+        private int mT;
+
+        private const string excelFile = "QuestionsData.xlsx";
+        private const string panelNamePrefix = "pA";
+        private const string buttonNamePrefix = "btnA";
+        private const string labelQPrefix = "lblQ";
+        private const string lblStatusFormat = "Current card is {0} of {1}";
+        private const string lblSetCorrectFormat = "This set correct matches: {0}";
+
+        private int qNumber;
+        private int qCorrect;
+
+        private int totalQuestions;
+        private Dictionary<string, string> questionSet;
+        private int currentSetCorrect;
+
+        public qR()
+        {
+            InitializeComponent();
+
+            this.ActiveControl = tbQ;
+        }
+
+        private void RegisterEventsToButton(Button b)
+        {
+            b.MouseDown += mD;
+            b.MouseUp += mU;
+            b.MouseMove += mM;
+        }
+
+        private void mD(object sender, MouseEventArgs e)
+        {
+            if (IsOver())
+                return;
+
+            if (sender.GetType() != typeof(Button))
+                return;
+
+            currb = sender as Button;
+            initX = MousePosition.X;
+            initY = MousePosition.Y;
+            mL = currb.Left;
+            mT = currb.Top;
+        }
+
+        private void mU(object sender, MouseEventArgs e)
+        {
+            if (IsOver())
+                return;
+
+            if (currb == null)
+                return;
+
+            var pName = panelNamePrefix + currb.Name.Split('A')[1];
+            var aPanel = (Panel)Controls.Find(pName, true).FirstOrDefault();
+            if (aPanel != null && currb.Left + 45 > aPanel.Left && currb.Right - 45 < aPanel.Right
+                                && currb.Top + 7 > aPanel.Top && currb.Bottom - 7 < aPanel.Bottom)
+            {
+                qCorrect++;
+
+                currb.Left = aPanel.Left + (aPanel.Width - currb.Width) / 2;
+                currb.Top = aPanel.Top + (aPanel.Height - currb.Height) / 2;
+            }
+            else
+            {
+                currb.Left = mL;
+                currb.Top = mT;
+            }
+
+            currb = null;
+
+            if (IsOver())
+            {
+                currentSetCorrect++;
+                MessageBox.Show("Great work!");
+                lblEnd.Text = "BRAVO!";
+                lblSetCorrect.Text = string.Format(lblSetCorrectFormat, currentSetCorrect);
+            }
+        }
+
+        private bool IsOver()
+        {
+            return qNumber == qCorrect;
+        }
+
+        private void mM(object sender, MouseEventArgs e)
+        {
+            if (IsOver())
+                return;
+
+            if (currb == null)
+                return;
+
+            currb.Left = mL + MousePosition.X - initX;
+            currb.Top = mT + MousePosition.Y - initY;
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            currentSetCorrect = 0;
+
+            lblEnd.Text = "";
+
+            ClearForm();
+
+            // read excel file and get questions
+            try
+            {
+                List<int> questionsNumbers = GetQuestionsNumbers();
+
+                if (questionsNumbers.Count > 0)
+                {
+                    lblSetCorrect.Text = string.Format(lblSetCorrectFormat, currentSetCorrect);
+                }
+                else
+                {
+                    lblSetCorrect.Text = "";
+                    lblStatus0.Text = "";
+
+                    return;
+                }
+
+                if (questionsNumbers.Count > 100)
+                {
+                    MessageBox.Show("Maximum number of questions is 100." + Environment.NewLine + "Check your input.");
+                    return;
+                }
+
+                Dictionary<string, string> QAndA = GetQuestionsFromFile(questionsNumbers);
+
+                questionSet = QAndA;
+
+                PutQuestionsOnForm(QAndA);
+
+                qNumber = QAndA.Keys.Count;
+                qCorrect = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+            PutQuestionsOnForm(questionSet);
+
+            lblEnd.Text = "";
+            qCorrect = 0;
+        }
+
+        private void ClearForm()
+        {
+            List<Control> toremove = new List<Control>();
+
+            foreach (Control c in this.Controls)
+            {
+                if (c.GetType() == typeof(Button))
+                {
+                    var b = c as Button;
+
+                    if (b.Name.StartsWith(buttonNamePrefix))
+                    {
+                        toremove.Add(c);
+                    }
+                }
+
+                if (c.GetType() == typeof(Label))
+                {
+                    var l = c as Label;
+                    if (l.Name.StartsWith(labelQPrefix))
+                    {
+                        toremove.Add(c);
+                    }
+                }
+
+                if (c.GetType() == typeof(Panel))
+                {
+                    var p = c as Panel;
+                    if (p.Name.StartsWith(panelNamePrefix))
+                    {
+                        toremove.Add(c);
+                    }
+                }
+            }
+
+            for (int i = 0; i < toremove.Count; i++)
+            {
+                this.Controls.Remove(toremove[i]);
+            }
+        }
+
+        private void PutQuestionsOnForm(Dictionary<string, string> QAndA)
+        {
+            Size panelSize = pTemplate.Size;
+            Size buttonSize = btnTemplate.Size;
+            Size lblSize = new Size(330, panelSize.Height);
+
+            int tp = pTemplate.Top + pTemplate.Size.Height + 6;
+            int lp = pTemplate.Left;
+
+            int tl = tp + 10;
+            int ll = 12;
+
+            int tb = tp;
+            int lb = btnTemplate.Left;
+
+            List<Button> answers = new List<Button>();
+
+            int c = 1;
+            // make them
+            foreach (var qa in QAndA)
+            {
+                var lblQ = new Label();
+                lblQ.Name = labelQPrefix + c;
+
+                var pan = new Panel();
+                pan.Name = panelNamePrefix + c;
+
+                var btn = new Button();
+                btn.Name = buttonNamePrefix + c;
+
+                btn.Font = new Font(btn.Font.FontFamily, 7);
+                btn.AutoSize = true;
+                btn.MaximumSize = new Size(panelSize.Width - 3, panelSize.Height - 3);
+
+                pan.Size = panelSize;
+                btn.Size = buttonSize;
+                btn.AutoSize = false;
+
+                lblQ.Text = qa.Key;
+                toolTip1.SetToolTip(lblQ, qa.Key);
+                btn.Text = qa.Value;
+                toolTip1.SetToolTip(btn, qa.Value);
+
+                lblQ.Top = tl;
+                lblQ.Left = ll;
+                lblQ.MaximumSize = lblSize;
+                lblQ.Size = lblSize;
+
+                pan.Top = tp;
+                pan.Left = lp;
+
+                pan.BorderStyle = BorderStyle.Fixed3D;
+                pan.BackColor = Color.Beige;
+
+                this.Controls.Add(lblQ);
+                this.Controls.Add(pan);
+
+                RegisterEventsToButton(btn);
+
+                answers.Add(btn);
+
+                // update positions
+                tp = tp + pTemplate.Size.Height + 6;
+                tl = tp + 10;
+                tb = tp;
+
+                c++;
+            }
+
+            answers.Shuffle();
+
+            // reset positions
+            tp = pTemplate.Top + pTemplate.Size.Height + 6;
+            tl = tp + 14;
+            tb = tp;
+
+            // put buttons
+            for (int i = 0; i < answers.Count; i++)
+            {
+                var b = answers[i];
+
+                b.Top = tb;
+                b.Left = lb;
+
+                // update positions
+                tp = tp + pTemplate.Size.Height + 6;
+                tl = tp + 14;
+                tb = tp;
+
+                // todo check problem with QUESTIONS labels
+                this.Controls.Add(b);
+                b.BringToFront();
+            }
+
+            lblStatus0.Text = string.Format(lblStatusFormat, answers.Count, totalQuestions);
+        }
+
+        private List<int> GetQuestionsNumbers()
+        {
+            var numsTmp = tbQ.Text.Split(',');
+
+            List<int> nums = new List<int>();
+            foreach (var n in numsTmp)
+            {
+                string trimmed = n.Trim();
+                if (trimmed.Contains('-'))
+                {
+                    string[] range = trimmed.Split('-');
+                    int start;
+                    int end;
+                    if (range.Length >= 2 && int.TryParse(range[0].Trim(), out start) && int.TryParse(range[1].Trim(), out end))
+                    {
+                        for (int i = start; i <= end; i++)
+                        {
+                            nums.Add(i);
+                        }
+                    }
+                }
+                else
+                {
+                    int a;
+                    if (int.TryParse(trimmed, out a))
+                    {
+                        nums.Add(a);
+                    }
+                }
+            }
+
+            return nums;
+        }
+
+        private Dictionary<string, string> GetQuestionsFromFile(List<int> questionsNumbers)
+        {
+            FileStream stream = File.Open(excelFile, FileMode.Open, FileAccess.Read);
+
+            // Reading from a OpenXml Excel file (2003 format; *.xls)
+            //IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+
+            // Reading from a OpenXml Excel file (2007 format; *.xlsx)
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            Dictionary<string, string> QAndA = new Dictionary<string, string>();
+
+            int curr = 1;
+            totalQuestions = 0;
+            // Data Reader methods
+            while (excelReader.Read())
+            {
+                if (questionsNumbers.Contains(curr) && curr <= questionsNumbers.Max())
+                {
+                    var cols = excelReader.FieldCount;
+                    var t1 = excelReader.GetValue(0);
+
+                    string q = excelReader.GetString(0);
+                    string a = excelReader.GetString(1);
+
+                    if (!QAndA.ContainsKey(q))
+                    {
+                        QAndA.Add(q, a);
+                    }
+                }
+
+                curr++;
+                totalQuestions++;
+            }
+
+            // Free resources (IExcelDataReader is IDisposable)
+            excelReader.Close();
+
+            return QAndA;
+        }
+    }
+}
